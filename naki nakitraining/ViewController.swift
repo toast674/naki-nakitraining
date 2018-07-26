f57a7aa2fa2e1ae4bf310bcafe2d70db8ea46e67
hiromasa <toast674@gmail.com>
2018-07-26 20:49:02 +0900

    Initial Commit

---
+++ naki nakitraining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  naki nakitraining
+//
+//  Created by 苅宿大將 on 2018/07/26.
+//  Copyright © 2018年 mizuno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
