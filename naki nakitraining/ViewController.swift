9679adc52549fe0c435e3921d6b86dfa3b7f39cd
hiromasa <toast674@gmail.com>
2018-07-29 20:21:43 +0900

    hello worldまで

--- naki nakitraining/ViewController.swift
+++ naki nakitraining/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
+extension UIColor {
+    static let normalText = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+}
+
+extension UIFont {
+    static let normalText = UIFont.systemFont(ofSize: 14)
+}
+
 class ViewController: UIViewController {
+    
+    let label = UILabel()
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        label.text = "麻雀鳴きトレーニング"
+        label.textAlignment = .center
+        button.titleLabel = "初級"
+        label.textColor = UIColor.normalText
+        label.font = UIFont.normalText
+        view.addSubview(label)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        label.frame = view.bounds
     }
 
     override func didReceiveMemoryWarning() {
